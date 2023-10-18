fbd544d66e6c5a2af173baccd5706ad4799e9f26
hassanuahmad <ahmad12p@uwindsor.ca>
2023-10-18 18:15:54 -0400

    api works

---
+++ client/app/dashboard/page.js
@@ -0,0 +1,124 @@
+"use client";
+import { useState } from "react";
+import { UserButton } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+    bio: z.string(),
+});
+
+export default function Dashboard() {
+    const [apiRes, setApiRes] = useState([]);
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    });
+
+    async function onSubmit(data) {
+        try {
+            const response = await fetch("http://localhost:3001/submit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ userInput: data.bio }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const responseData = await response.json();
+            // console.log("responseData", responseData.result.keywords);
+            setApiRes(responseData.result.keywords);
+        } catch (error) {
+            console.error(
+                "There was a problem with the fetch operation:",
+                error.message
+            );
+        }
+    }
+
+    return (
+        <>
+            <header className="bg-gray-50">
+                <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+                    <div className="flex items-center justify-end gap-4">
+                        <UserButton afterSignOutUrl="/" />
+                    </div>
+
+                    <div className="mt-8">
+                        <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                            Welcome Back, Barry!
+                        </h1>
+
+                        <p className="mt-1.5 text-sm text-gray-500">
+                            Your website has seen a 52% increase in traffic in
+                            the last month. Keep it up! 🚀
+                        </p>
+                    </div>
+                </div>
+            </header>
+
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="w-2/3 space-y-6"
+                >
+                    <FormField
+                        control={form.control}
+                        name="bio"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Bio</FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                        placeholder="Tell us a little bit about yourself"
+                                        className="resize-none"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription>
+                                    You can <span>@mention</span> other users
+                                    and organizations.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+
+            {apiRes.length > 0 && (
+                <div className="mt-8">
+                    <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                        Response
+                    </h1>
+                    {apiRes.map((item, index) => (
+                        <div key={index}>
+                            <p>{item.text}</p>
+                            <p>{item.relevance}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
+    );
+}
--- server/index.js
+++ server/index.js
@@ -1,57 +1,64 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const port = 3001;
 
+const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+const { IamAuthenticator } = require("ibm-watson/auth");
 
-//Integration of NLP - 1st attempt
-const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
+// Configurations
+app.use(cors());
+app.use(express.json()); // Use this to parse incoming JSON data
 
+// IBM Watson NLP Setup
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-  version: '2022-04-07',
-  authenticator: new IamAuthenticator({
-    apikey: 'gx6kRGAf7X3a0A4y9WYcznh0WZlkPHEmDH6M5hIEal8c',
-  }),
-  serviceUrl: 'https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/4fe634a9-b7bc-41a6-b68b-a5ad7970fba4',
+    version: "2022-04-07",
+    authenticator: new IamAuthenticator({
+        apikey: process.env.NLP_API_KEY,
+    }),
+    serviceUrl:
+        "https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/4fe634a9-b7bc-41a6-b68b-a5ad7970fba4",
 });
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.render('index');
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello, this is the NLP API server!");
 });
 
-app.post('/submit', (req, res) => {
-  const userInput = req.body.userInput;
-  const userInputEncoded = encodeURIComponent(userInput);
-  // Define the analyzeParams using the user input
-  const analyzeParams = {
-    text: userInputEncoded,
-    features: {
-      entities: {
-        emotion: true,
-        sentiment: true,
-        limit: 10,
-      },
-      keywords: {
-        emotion: true,
-        sentiment: true,
-        limit: 10,
-      },
-    },
-  };
+app.post("/submit", (req, res) => {
+    const userInput = req.body.userInput;
+    const userInputEncoded = encodeURIComponent(userInput);
 
-  // Call NLU service to analyze the user input
-  naturalLanguageUnderstanding.analyze(analyzeParams)
-      .then(analysisResults => {
-        res.render('results', { analysisResults }); // Render the results template
-      })
-      .catch(err => {
-        console.log('error:', err);
-        res.status(500).send('An error occurred during analysis.');
-      });
+    // Define the analyzeParams using the user input
+    const analyzeParams = {
+        text: userInputEncoded,
+        features: {
+            entities: {
+                emotion: true,
+                sentiment: true,
+                limit: 10,
+            },
+            keywords: {
+                emotion: true,
+                sentiment: true,
+                limit: 10,
+            },
+        },
+    };
+
+    // Call NLU service to analyze the user input
+    naturalLanguageUnderstanding
+        .analyze(analyzeParams)
+        .then((analysisResults) => {
+            res.json(analysisResults);
+        })
+        .catch((err) => {
+            console.log("error:", err);
+            res.status(500).send("An error occurred during analysis.");
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
